5cde6cc398132f8e511b91e218c8be90a372ac36
surjit8 <68872161+surjit8@users.noreply.github.com>
2022-04-03 16:16:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+canvas = document.getElementById("Mycanvas")
+ctx = canvas.getContext("2d")
+
+color = "red"
+
+ctx.beginPath()
+ctx.strokeStyle = color
+ctx.lineWidth = 2
+ctx.arc(200 , 200 , 40 , 0 , 2*Math.PI)
+ctx.stroke();
+
+canvas.addEventListner("mousedown" , my_mousedown);
+function my_mousedown (e){
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+    circle(mouse_x , mouse_y)
+
+}
+
+function circle(mouse_x , mouse_y){
+    ctx.beginPath()
+    ctx.strokeStyle = color
+    ctx.lineWidth = 2
+    ctx.arc(mouse_x , mouse_y , 40 , 0 , 2*Math.PI)
+    ctx.stroke();
+}
